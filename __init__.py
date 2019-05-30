ba1c48e11b634348777bcfaf7d3eac85b34927f7
Justin <justin.seiser@rackspace.com>
2019-05-30 16:16:30 -0400

    working on search by account_id

--- __init__.py
+++ __init__.py
@@ -28,7 +28,7 @@ class ASSkill(Skill):
                 data = await resp.json()
                 return data["#collection"]
 
-    async def _get_account_id_by_name(self, environment, name):
+    async def _get_customer_id_by_name(self, environment, name):
         account_list = await self._get_accounts(environment)
         for account in account_list:
             if name.lower() == account["name"].lower():
@@ -38,7 +38,7 @@ class ASSkill(Skill):
         return None
 
     async def _get_account_by_name(self, environment, name):
-        customer_id = await self._get_account_id_by_name(environment, name)
+        customer_id = await self._get_customer_id_by_name(environment, name)
         if customer_id:
             sslcontext = ssl.create_default_context(
                 cafile=self.config["sites"][environment]["ca"]
@@ -70,6 +70,33 @@ class ASSkill(Skill):
                 if not resp.status == 200:
                     return None
                 return data["#item"]
+
+    async def _get_customer_id_by_account_id(self, environment, account_id):
+        account_list = await self._get_accounts(environment)
+        for account in account_list:
+            for environment in account["environments"]:
+                if account_id == environment["account_id"]:
+                    return account["id"]
+        return None
+
+    async def _get_account_by_acount_id(self, environment, account_id):
+        customer_id = await self._get_customer_id_by_account_id(environment, account_id)
+        if customer_id:
+            sslcontext = ssl.create_default_context(
+                cafile=self.config["sites"][environment]["ca"]
+            )
+            sslcontext.load_cert_chain(self.config["sites"][environment]["cert"])
+            timeout = aiohttp.ClientTimeout(total=60)
+            api_url = (
+                f"{self.config['sites'][environment]['url']}/customers/{customer_id}"
+            )
+
+            async with aiohttp.ClientSession(timeout=timeout) as session:
+                async with session.get(api_url, ssl=sslcontext) as resp:
+                    data = await resp.json()
+                    return data["#item"]
+        else:
+            return None
 
     # Beging Matching functions
 
@@ -108,12 +135,29 @@ class ASSkill(Skill):
             await message.respond(f"{return_text}")
 
     @match_regex(
-        r"^account services (?P<environment>\w+-\w+|\w+) get account id: (?P<customer_id>.*)$"
+        r"^account services (?P<environment>\w+-\w+|\w+) get account customer_id: (?P<customer_id>.*)$"
     )
     async def get_account_by_customer_id(self, message):
         environment = message.regex.group("environment")
         customer_id = message.regex.group("customer_id")
         account = await self._get_account_by_customer_id(environment, customer_id)
+        return_text = f"*{environment} - Account*\n"
+        if account:
+            return_text = f"{return_text}```Customer Name: {account['name']}\nCustomer ID: {account['id']}```\n"
+            for environment in account["environments"]:
+                return_text = f"{return_text}```Environment\n\tID: {environment['id']}\n\tType: {environment['env_type']}\n\tAccount ID: {environment['account_id']}\n\tSub Account ID: {environment['subaccount_id']}```"
+            await message.respond(f"{return_text}")
+        else:
+            return_text = f"{return_text}```No Match```"
+            await message.respond(f"{return_text}")
+
+    @match_regex(
+        r"^account services (?P<environment>\w+-\w+|\w+) get account account_id: (?P<account_id>.*)$"
+    )
+    async def get_account_by_account_id(self, message):
+        environment = message.regex.group("environment")
+        account_id = message.regex.group("account_id")
+        account = await self._get_account_by_account_id(environment, account_id)
         return_text = f"*{environment} - Account*\n"
         if account:
             return_text = f"{return_text}```Customer Name: {account['name']}\nCustomer ID: {account['id']}```\n"
