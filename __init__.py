3ff2384854d13489f747d898b46350e4baf8ef97
Justin <justin.seiser@rackspace.com>
2019-05-30 19:43:08 -0400

    seeing if i can pass in status active

--- __init__.py
+++ __init__.py
@@ -123,7 +123,7 @@ class ASSkill(Skill):
         timeout = aiohttp.ClientTimeout(total=60)
         api_url = f"{self.config['sites'][environment]['url']}/customers"
         async with aiohttp.ClientSession(timeout=timeout) as session:
-            payload = {"#item": {"name": name, "products": []}}
+            payload = {"#item": {"name": name, "products": [], "status": "active"}}
             headers = {"content-type": "application/x.shr+json"}
             async with session.post(
                 api_url, ssl=sslcontext, json=payload, headers=headers
