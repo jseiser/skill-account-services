4d0d8a79d06747c9d1534523ea8cc9f800170013
Justin <justin.seiser@rackspace.com>
2019-05-30 13:28:15 -0400

    playing with customers

--- __init__.py
+++ __init__.py
@@ -29,7 +29,7 @@ class ASSkill(Skill):
                 #     return_text = (
                 #         f"{return_text}```ID: {i['id']} Name: {i['name']}```\n"
                 #     )
-                print(data["#collection"])
+                print(data["#collection"][0])
                 return data
 
     # Beging Matching functions
